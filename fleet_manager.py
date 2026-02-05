4d21d877c2650ddb84bb8ded5f47134655fc0eff
Dovydas Gruzinskas <dg525@kent.ac.uk>
2026-02-05 18:28:21 +0000

    first function

---
+++ fleet_manager.py
@@ -0,0 +1,12 @@
+def init_database():
+    n = ["Jean-luc Picard", "William Riker", "Data", "Beverly Crusher", "Geordi La Forge"]
+    r = ["Captain", "Commander", "Lt. Commander", "Commander", "Lieutenant"]
+    d = ["Command", "Command", "Operations", "Sciences", "Operations"]
+    i = ["SP-937-215", "SC-231-427", "OP-411-872", "SC-552-904", "OP-774-316"]
+    
+    return n, r, d, i
+
+
+
+
+    
