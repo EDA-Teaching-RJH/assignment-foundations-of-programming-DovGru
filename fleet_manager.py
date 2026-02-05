b2aba62a3df5a441ce461b856ad557ec4f99f618
Dovydas Gruzinskas <dg525@kent.ac.uk>
2026-02-05 18:47:24 +0000

    ID fromat change

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,10 +1,12 @@
 def init_database():
-    n = ["Jean-luc Picard", "William Riker", "Data", "Beverly Crusher", "Geordi La Forge"]
+    n = ["Jean-luc Picard", "William Riker", "Data", "Beverly Crusher", "Geordi La Forge"]                       #a function with parallel lists with all information
     r = ["Captain", "Commander", "Lt. Commander", "Commander", "Lieutenant"]
     d = ["Command", "Command", "Operations", "Sciences", "Operations"]
-    i = ["SP-937-215", "SC-231-427", "OP-411-872", "SC-552-904", "OP-774-316"]
+    i = ["ID1", "ID2", "ID3", "ID4", "ID5"]
     
     return n, r, d, i
+
+def display_menu():
 
 
 
